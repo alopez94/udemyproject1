c14706ea07b3e1c64422bd525fec402f4f750f40
alopez94 <allan.u94l@gmail.com>
2021-02-19 23:10:34 -0600

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//router
+
+import {AboutComponent} from './about/about.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+
+const routes: Routes = [
+
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
